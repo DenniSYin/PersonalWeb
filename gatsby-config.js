06a7363ea6f50c928c9c085f2373bf54e1b08593
DenniSYin <yinfamin@hotmail.com>
2019-07-10 07:19:32 +1000

    4th commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,11 @@
 
 module.exports = {
   /* Your site config here */
+  siteMetadata: {
+    title: 'Full-Stack Bootcamp',
+    author: 'Denis YIN.'
+  },
   plugins: [
-      'gatsby-plugin-sass'
+    'gatsby-plugin-sass'
   ]
 }
